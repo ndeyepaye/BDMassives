6439440f951aa611dec1dd6bf784dd2984d32d47
ndeye <mamipaye@gmail.com>
2019-12-06 02:42:16 -0500

    Code de l'obtention d'un point de depart pas teste vu que le projet ne compile pas

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from pymongo import MongoClient
 from py2neo import Graph
 import json
+# import random
 
 import os
 MONGO_PORT = 27017
@@ -36,20 +37,42 @@ def hearbeat():
     
     return jsonify ({
         "nb_restaurants":nb_restaurants,
-         "total_path_length":total_path_length
-         })
+        "total_path_length": total_path_length
+    })
+
 
 @application.route("/type", methods=["GET"])
-def typeRestaurants():  
+def typeRestaurants():
     print("ioiiii")
-    type_restaurants = mongo_client['velo']['cat_restaurants'].find({},{"_id":0, "Nom":1})
+    type_restaurants = mongo_client['velo']['cat_restaurants'].find({}, {"_id": 0, "Nom": 1})
 
-    response =[item['Nom']for item in type_restaurants]
-    return json.dumps(response, ensure_ascii=False,  indent="\t")
-    
-#x =json.dumps(rep, skipkeys=False,  check_circular=True, allow_nan=True, cls=None, indent=1, separators=None, default=None, sort_keys=False)
+    response = [item['Nom'] for item in type_restaurants]
+    return json.dumps(response, ensure_ascii=False, indent="\t")
+
+
+# x@application.route("/starting-point", methods=["GET"])
+# xdef startingPoint():
+# x   req_data = request.get_json()
+# x
+# x   listTypeResto = req_data['type'][0]
+# maxLengh= req_data['maximum_length']
+# maxLengh+= maxLengh*0.1
+# x  type_restaurants = mongo_client['velo']['restaurants'].find({ "type": { "$in": listTypeResto } }, {"_id": 0, "coordinates": 1})
+
+# nombreRandom= random.randint(1, len(type_restaurants))
+# pointDepart =  type_restaurants[nombreRandom]
+
+# query = "MATCH (b:Intersection) WITH b, distance(point(b), point({{latitude:{0}, longitude:{1}}})) AS dist RETURN b.x AS lat,b.y AS lon, dist ORDER BY dist".format(
+#  pointDepart.Latitude, pointDepart.Longitude)
+# result = neo4j_graph.run(query).data()
+# if result.records[0].dist <= maxLengh:
+#  jsonify({
+#     "starting_point": {"type":"Point", "coordinates":[result.records[0].lat, result.records[0].lon]}
+
+
+#  })
+
+
+# x =json.dumps(rep, skipkeys=False,  check_circular=True, allow_nan=True, cls=None, indent=1, separators=None, default=None, sort_keys=False)
 
 application.run('0.0.0', APP_PORT, debug=True)
-
-
-
