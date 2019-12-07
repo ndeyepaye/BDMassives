59bc4b5887a03347134f9f9c358a54a2496ba2d8
ndeye <mamipaye@gmail.com>
2019-12-07 05:06:23 -0500

    requete fonctionnel point de depart proche du restaurant selon les types de maniere aletoire

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify
 from pymongo import MongoClient
-from py2neo import Graph
+from py2neo import Graph,Node,Relationship
 import json
-# import random
+from flask import request
+import random
 
 import os
 MONGO_PORT = 27017
@@ -50,29 +51,30 @@ def typeRestaurants():
     return json.dumps(response, ensure_ascii=False, indent="\t")
 
 
-# x@application.route("/starting-point", methods=["GET"])
-# xdef startingPoint():
-# x   req_data = request.get_json()
-# x
-# x   listTypeResto = req_data['type'][0]
-# maxLengh= req_data['maximum_length']
-# maxLengh+= maxLengh*0.1
-# x  type_restaurants = mongo_client['velo']['restaurants'].find({ "type": { "$in": listTypeResto } }, {"_id": 0, "coordinates": 1})
+@application.route("/starting-point", methods=["GET"])
+def startingpoint():
+     req_data = request.json
+     list_typeresto = req_data['type']
+     max_lengh= req_data['maximum_length']
+     cat_restaurants = mongo_client['velo']['cat_restaurants'].find({"Nom": {"$in": list_typeresto}}, {"ID": 1, "Nom": 1} )
+     match_typeresto =[str(int(item['ID']))for item in cat_restaurants]
+     max_lengh+= max_lengh*0.1
+     type_restaurants = mongo_client['velo']['restaurants'].find({"Categories": {"$in": match_typeresto}}, {"Nom": 1, "Latitude": 1, "Longitude": 1})
+     response = [[item['Latitude'],item['Longitude']] for item in type_restaurants]
 
-# nombreRandom= random.randint(1, len(type_restaurants))
-# pointDepart =  type_restaurants[nombreRandom]
+     nombreRandom= random.randint(1, len(response))
+     resto_depart =response[nombreRandom]
+     query = "MATCH (b:Intersection) WITH b, distance(point(b), point({{latitude:{0}, longitude:{1}}})) AS dist RETURN b.x AS lat,b.y AS lon, dist ORDER BY dist".format(
+     resto_depart[0], resto_depart[1])
+     return {
+         "starting_point": {"type": "Point", "coordinates": resto_depart}
+     }
+     #result = neo4j_graph.run(query).data()
+    #if result.records[0].dist <= max_lengh:
+    #return {
+     #     "starting_point": {"type":"Point", "coordinates":[result.records[0].lat, result.records[0].lon]}
+     #}
 
-# query = "MATCH (b:Intersection) WITH b, distance(point(b), point({{latitude:{0}, longitude:{1}}})) AS dist RETURN b.x AS lat,b.y AS lon, dist ORDER BY dist".format(
-#  pointDepart.Latitude, pointDepart.Longitude)
-# result = neo4j_graph.run(query).data()
-# if result.records[0].dist <= maxLengh:
-#  jsonify({
-#     "starting_point": {"type":"Point", "coordinates":[result.records[0].lat, result.records[0].lon]}
 
-
-#  })
-
-
-# x =json.dumps(rep, skipkeys=False,  check_circular=True, allow_nan=True, cls=None, indent=1, separators=None, default=None, sort_keys=False)
 
 application.run('0.0.0', APP_PORT, debug=True)
